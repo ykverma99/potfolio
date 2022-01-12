d9a1d1d33a1497d89c532983c7ce060b64838ba4
ykverma99 <74640546+ykverma99@users.noreply.github.com>
2022-01-13 02:08:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const ball = document.getElementById('ball')
 const label = document.getElementById('label')
 const line = document.getElementsByClassName('nav__center-container-line')
 
+// variable for fade in-out
+const about = document.getElementById('about');
+const capability = document.getElementById('capa')
+const meetChat = document.getElementById('meetchat');
+const homebase = document.getElementById('homebase');
+const rps = document.getElementById('rps');
+const drum = document.getElementById('drum');
+const playing = document.getElementById('playing');
+const cursor = document.getElementById('cursor');
+const jacpiro = document.getElementById('jackpiro');
+const gym = document.getElementById('gym');
 
 
 // making hamburger
@@ -24,27 +35,94 @@ input.addEventListener('change',()=>{
     }
 })
 
-function scrollAppear(){
-
-// variable for fade in-out
-const about = document.getElementById('about');
-const capability = document.getElementById('capa')
-
-// fade in out on about
-
-const aboutPosition = about.getBoundingClientRect().top;
-const capabilityPosition = capability.getBoundingClientRect().top;
-const height = window.innerHeight / 1.2;
-
-if(aboutPosition<height){
-    about.classList.add('appear')
-}
-if(capabilityPosition<height){
-    capability.classList.add('appear')
-}
-}
-
-window.addEventListener('scroll', scrollAppear)
 
 
 
+// function for scrolling animation
+
+window.addEventListener('scroll', ()=>{
+    
+    // fade in out on about
+    const aboutPosition = about.getBoundingClientRect().top;
+    const capabilityPosition = capability.getBoundingClientRect().top;
+    const jacpiroPosition = jacpiro.getBoundingClientRect().top;
+    const playingPosition = playing.getBoundingClientRect().top;
+    const cursorPosition = cursor.getBoundingClientRect().top;
+    const homeBasePosition = homebase.getBoundingClientRect().top;
+    const meetChatPosition = meetChat.getBoundingClientRect().top;
+    const drumPosition = drum.getBoundingClientRect().top;
+    const rpsPosition = rps.getBoundingClientRect().top;
+    const gymPosition = gym.getBoundingClientRect().top;
+
+    // height of window
+    const height = window.innerHeight / 1.2;
+
+    if(aboutPosition<height){
+        about.classList.add('appear')
+    }
+
+    if(capabilityPosition<height){
+        capability.classList.add('appear')
+    }
+
+    if(jacpiroPosition<height){
+        jacpiro.classList.add('appear');
+    }
+    if(homeBasePosition<height){
+        homebase.classList.add('appear');
+    }
+    if(playingPosition<height){
+        playing.classList.add('appear');
+    }
+    if(drumPosition<height){
+        drum.classList.add('appear');
+    }
+    if(meetChatPosition<height){
+        meetChat.classList.add('appear');
+    }
+    if(rpsPosition<height){
+        rps.classList.add('appear');
+    }
+    if(gymPosition<height){
+        gym.classList.add('appear');
+    }
+    if(cursorPosition<height){
+        cursor.classList.add('appear');
+    }
+})
+
+
+
+// function for onpen page link 
+
+meetChat.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/Meet-chat/')
+})
+
+jacpiro.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/first-project/')
+})
+
+homebase.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/homebase/')
+})
+
+cursor.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/fun-game/')
+})
+
+playing.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/css-js-variables/')
+})
+
+drum.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/drum-kit/')
+})
+
+rps.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/rock-paper-scissor/')
+})
+
+gym.addEventListener('click',()=>{
+    window.open('https://ykverma99.github.io/Gym/')
+})
